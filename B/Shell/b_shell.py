0f8a33e61f967012afcbdc949942977cf0e54d55
venombash <45216357+venombash@users.noreply.github.com>
2019-04-20 16:51:54 -0500

    Add files via upload
    Shell Created!

--- B/Shell/b_shell.py
+++ B/Shell/b_shell.py
@@ -1,16 +1,13 @@
-# b_compile.py
+# b_shell.py
 # Ben Fellers 2019
-# 
-# For language B
+# For B language
 
-import time
-import random
-import sys
 import os
+import random
 
 variables = {}
-         
-def main(file_name):
+
+def compile_script(file_name):
    #file_name = input("Enter path of file to be complied: ") # User enters the path of the script to be compiled
    with open(file_name, "r") as f:
       # we have opened the file to read
@@ -72,7 +69,7 @@ def main(file_name):
          elif "math" in line and "@" not in line:
             print(eval(line[5:]))
          elif "use" in line and "@" not in line:
-            main(line[4:])
+            compile_script(line[4:])
          elif "yield" in line and "command" in line and "@" not in line:
             os.system(line[14:])
          elif "read" in line and "@" not in line:
@@ -80,12 +77,70 @@ def main(file_name):
             print(file_to_read.read())
             file_to_read.close()
             input("Press enter to continue...")
-            
-file_name = input("Enter name of file to be compiled: ")
-   
+
+def main():
+   while True:
+      command = input("> ")
+      if "print" in command and "printvar" not in command:
+         print(command[6:])
+      elif command == "exit":
+         exit()
+      elif "raw_input" in command and "raw_input$" not in command:
+         variables[command[10:11]] = input(command[12:])
+         #print(variables)
+      elif "printvar" in command:
+         print(variables[command[9:10]])
+      elif "raw_input$" in command:
+         variables[command[11:12]] = int(input(command[13:]))
+         #print(variables)
+      elif "yield" in command and "command" in command:
+         os.system(command[14:])
+      elif "var" in command and "int" in command:
+         variables[command[8:9]] = int(command[10:])
+         #print(variables)
+      elif "var" in command and "float" in command:
+         variables[command[10:11]] = float(command[12:])
+      elif "var" in command and "string" in command:
+         variables[command[11:12]] = str(command[13:])
+      elif "var" in command and "bool" in command:
+         variables[command[9:10]] = bool(command[11:])
+      elif "var" in command and "rand" in command:
+         variables[command[9:10]] = random.randint(1, 10)
+      elif "math" in command:
+         print(eval(command[5:]))
+      elif "use" in command:
+         compile_script(command[4:])
+      elif "read" in command:
+         file_to_read = open(command[5:], "r")
+         print(file_to_read.read())
+         file_to_read.close()
+         input("Press enter to continue...")
+      elif command == "help":
+         print("""
+         print - Prints a string | print Hello World!
+         raw_input - Grabs the users answer and stores it in a variable | raw_input v What's your name?: 
+         printvar - Prints a variable | printvar v
+         raw_input$ - Grabs the users answer and stores it as a integer | raw_input$ i Enter number: 
+         yield command - Runs a command in the CMD / Terminal | yield command echo "Hello World!"
+         var (int, float, string, bool, rand) - Creates a variable | var int a 1
+         math - Calculates a math expression | math 1 + 1
+         use - Runs a .b script | use /B_Scripts/hello_world.b
+         read - Reads a file | read /B/Compiler/b_compile.py
+         exit - Exits the shell
+         """
+         )
+      else:
+         print("Command %s not found" % (command))
+         
 if __name__ == "__main__":
    try:
-      os.system("title " + file_name)
+      os.system("title Shell")
    except:
       print("running on linux/mac name change fail...")
-   main(file_name)
+   print("""
+   B Shell
+   Ben Fellers 2019
+   V 1.0.0
+   """
+   )
+   main()
